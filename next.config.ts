b190e923d146786dc208c58597634d78e2be5695
karan123567 <karasingaur600@gmail.com>
2026-08-08 11:49:02 +0530

    Next js 15 problem

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["firebase-admin"],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'picsum.photos' },
